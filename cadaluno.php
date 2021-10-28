35bb3d3c4125a84a7d17b3ee501971006146ea3d
Viviane Meneguin <vivianemeneguin@hotmail.com>
2021-10-28 15:02:05 -0300

    trabalho facul

---
+++ cadaluno.php
@@ -0,0 +1,27 @@
+<?php
+
+echo '<h1> Utilizando o array global $_GET </h1>';
+echo '<h3>';
+echo $_GET["txtMatricula"];
+echo '</h3>';
+echo '<h3>';
+echo $_GET["txtNomeAlun"];
+echo '</h3>';
+echo '<h3>';
+echo $_GET["txtNomeMae"];
+echo '</h3>';
+echo '<h3>';
+echo $_GET["txtDataNasc"];
+echo '</h3>';
+echo '<h3>';
+echo $_GET["txtEnd"];
+echo '</h3>';
+echo '<h3>';
+echo $_GET["txtCidade"];
+echo '</h3>';
+echo '<h3>';
+echo $_GET["txtCep"];
+echo '</h3>';
+echo '<h3>';
+
+?>
---
+++ caddisciplina.php
@@ -0,0 +1,27 @@
+<?php
+
+//echo '<h1> Hello World - Teste PHP </h1>';
+//echo '<br>';
+//echo '<br>';
+//echo '<br>';
+//echo '<br>';
+//echo '<h1> Dados dos formulário de cadastro </h1>';
+
+echo '<h1> Utilizando o array global $_POST </h1>';
+echo '<h2>';
+echo $_POST["txtCodigo"];
+echo '</h2>';
+echo '<h2>';
+echo $_POST["txtNomeDisc"];
+echo '</h2>';
+
+
+//echo '<h1> Utilizando o array global $_GET </h1>';
+//echo '<h2>';
+//echo $_GET["txtCodigo"];
+//echo '</h2>';
+//echo '<h2>';
+//echo $_GET["txtNomeDisc"];
+//echo '</h2>';
+
+?>
